1bdf9074fc4d0bc1c4ab845fb20da09fb90bfb6e
julia <julia0dabrowka@gmail.com>
2021-09-28 17:57:52 +0200

    correct

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -21,10 +21,12 @@
         <div class="bottom-bar">
             <div class="play-options">
                 <font-awesome-icon icon="share-alt" :style="{ color : 'purple', backgroundColor: 'white', padding: '15px', borderRadius: '50%' }" />
-                <font-awesome-icon icon="step-backward" :style="{ color: 'white', backgroundColor: 'purple', marginLeft: '15%', padding: '25px', borderRadius: '50%' }" @click='previous' />
-                <font-awesome-icon icon="play" :style="{ color: 'white', backgroundColor: 'purple', margin: '0 3%', padding: '35px', borderRadius: '50%' }" @click="pressedPlay" v-if="button" />
-                <font-awesome-icon icon="pause" :style="{ color: 'white', backgroundColor: 'purple', margin: '0 3%', padding: '35px', borderRadius: '50%' }" @click="pressedPlay" v-else />
-                <font-awesome-icon icon="step-forward" :style="{ color: 'white', backgroundColor: 'purple', marginRight: '15%', padding: '25px', borderRadius: '50%' }" @click='next' />
+                <div class="buttons">
+                  <font-awesome-icon icon="step-backward" :style="{ color: 'white', backgroundColor: 'purple', padding: '25px', borderRadius: '50%' }" @click='previous' />
+                  <font-awesome-icon icon="play" :style="{ color: 'white', backgroundColor: 'purple', margin: '0 2%', padding: '35px', borderRadius: '50%' }" @click="pressedPlay" v-if="button" />
+                  <font-awesome-icon icon="pause" :style="{ color: 'white', backgroundColor: 'purple', margin: '0 2%', padding: '35px', borderRadius: '50%' }" @click="pressedPlay" v-else />
+                  <font-awesome-icon icon="step-forward" :style="{ color: 'white', backgroundColor: 'purple', padding: '25px', borderRadius: '50%' }" @click='next' />
+                </div>
                 <font-awesome-icon icon="heart" :style="{ color: 'red', padding:'15px', backgroundColor: 'white', borderRadius: '50%' }" />
             </div>
         </div>
